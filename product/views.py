2216f32e077879effb2b44d344069190172c1ae9
gitsuhyeok05 <jeffkby@gmail.com>
2025-04-19 02:16:13 +0900

    3주차 django 개발 완료

--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 #from django.http import HttpResponse
 from .models import MainContent
 
@@ -7,4 +7,7 @@ def index(request):
     context = {'content_list': content_list}
     return render(request, 'product/content_list.html',context)
 
-    #return HttpResponse("Hello World")
+def detail(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list':content_list}
+    return render(request, 'product/content_detail.html', context)
